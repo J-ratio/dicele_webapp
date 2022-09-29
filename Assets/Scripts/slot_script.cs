f4222b05ac22df85b839883ff8defd2340660be9
r_@tio <86887365+J-ratio@users.noreply.github.com>
2022-09-29 08:01:28 +0530

    Revert "added green border on selection logic"
    This reverts commit d94aa0df1d8ce0d9731cf236a8006d3884f14abe.

--- Assets/Scripts/slot_script.cs
+++ Assets/Scripts/slot_script.cs
@@ -13,8 +13,6 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
     public Button play_btn;
     [SerializeField]
     private Sprite play_btn_enabled_sprite;
-    [SerializeField]
-    private GameObject greenborder;
 
     //Gamevariables
     public static bool slot_tag; //is true when a spawn dice is selected before slot is selected
@@ -25,10 +23,9 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
     public static int[,] board_dice_count = new int[5,6]; //stores the frequency of dice numbers placed on board column wise; eg: if there are 2 die5 placed on 1st column then board_dice_count[0,4] = 2;
     public static int [,] green_dice_count = new int[5,6]; //stores matched dice numbers on board column wise; eg: if the 2 die4 images are matcched in 1st column then green_dice_count[0,3] = 2;
     public static int[,] Ref_board_dice_count = {{0,0,0,1,2,2},{2,0,1,1,1,0},{1,2,1,0,0,1},{1,1,2,0,1,0},{2,0,0,0,1,2}}; //stores the dice number frequency column wise in solution ie Rsf_array
-    public static Color[,] pixel_array = {{new Color(231/255f,230/255f,233/255f) , new Color(67/255f,84/255f,219/255f) , new Color(67/255f,84/255f,219/255f) , new Color(67/255f,84/255f,219/255f) , new Color(231/255f,230/255f,233/255f)},{ new Color(67/255f,84/255f,219/255f) , new Color(67/255f,84/255f,219/255f) , new Color(42/255f,38/255f,49/255f) , new Color(67/255f,84/255f,219/255f) , new Color(251/255f,209/255f,15/255f)},{ new Color(67/255f,84/255f,219/255f) , new Color(67/255f,84/255f,219/255f) , new Color(255/255f,0/255f,0/255f) , new Color(67/255f,84/255f,219/255f) , new Color(251/255f,209/255f,15/255f)},{ new Color(67/255f,84/255f,219/255f) , new Color(67/255f,84/255f,219/255f) , new Color(42/255f,38/255f,49/255f) , new Color(67/255f,84/255f,219/255f) , new Color(251/255f,209/255f,15/255f)},{ new Color(231/255f,230/255f,233/255f) , new Color(67/255f,84/255f,219/255f) , new Color(67/255f,84/255f,219/255f) , new Color(67/255f,84/255f,219/255f) , new Color(231/255f,230/255f,233/255f)}};
+    public static Color[,] pixel_array =  {{new Color(165/255f,36/255f,50/255f) , new Color(224/255f,85/255f,41/255f) , new Color(234/255f,171/255f,62/255f) , new Color(243/255f,215/255f,90/255f) , new Color(108/255f,200/255f,128/255f)},{new Color(224/255f,85/255f,41/255f) , new Color(234/255f,171/255f,62/255f) , new Color(243/255f,215/255f,90/255f) , new Color(108/255f,200/255f,128/255f) , new Color(145/255f,230/255f,241/255f)},{new Color(234/255f,171/255f,62/255f) , new Color(243/255f,215/255f,90/255f) , new Color(108/255f,200/255f,128/255f) , new Color(145/255f,230/255f,241/255f) , new Color(112/255f,95/255f,245/255f)},{new Color(243/255f,215/255f,90/255f) , new Color(108/255f,200/255f,128/255f) , new Color(145/255f,230/255f,241/255f) , new Color(112/255f,95/255f,245/255f) , new Color(116/255f,41/255f,153/255f)},{new Color(108/255f,200/255f,128/255f) , new Color(145/255f,230/255f,241/255f) , new Color(112/255f,95/255f,245/255f) , new Color(116/255f,41/255f,153/255f), new Color(163/255f,82/255f,186/255f)}};
     public static int Total_play; //counter for no. of times play is pressed
     public static int matched; //counter for no. of dice matched on the board
-    public static bool enable_play = false;
 
 
 
@@ -39,8 +36,6 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
             board_dice_count[slot_pos[0],slot_dice_script.selected_slot_dice_number] ++;        
             dice.GetComponent<SpriteRenderer>().sprite = slot_dice_sprite;
             dice_slot_tag = true;
-            slot_dice_script.Yellow_selected = false;
-            slot_dice_script.White_selected = false;
             dice.SetActive(true);
             dice.GetComponent<slot_dice_script>().dice_number = slot_dice_script.selected_slot_dice_number;
 
@@ -55,12 +50,9 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
                     }
                 }
             dice.GetComponent<slot_dice_script>().Correct_row = slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().Correct_row;
-            dice.GetComponent<slot_dice_script>().Correct_column = slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().Correct_column; 
-            if(slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().isboping){
-                slot_dice_script.dice_moved++;
+            dice.GetComponent<slot_dice_script>().Correct_column = slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().Correct_column;            
             }
-            slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().Stop_boping();         
-            }
+
         else if(dice.GetComponent<SpriteRenderer>().sprite == null && dice_script.dice_tag == true)
             {
             board_dice_count[slot_pos[0],dice_script.selected_dice_number] ++;
@@ -68,16 +60,6 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
             slot_tag = true;
             dice.SetActive(true);
             dice.GetComponent<slot_dice_script>().dice_number = dice_script.selected_dice_number;
-            dice.GetComponent<slot_dice_script>().Stop_boping();
-            dice_script.dice_onboard ++;
-            enable_play = true;
-            }
-
-        check_enable_playBtn();
-    }
-
-    public void check_enable_playBtn(){
-        if(slot_dice_script.dice_moved == (dice_script.dice_onboard) && enable_play){
             play_btn.GetComponent<Button>().enabled = true;
             play_btn.GetComponent<Image>().sprite = play_btn_enabled_sprite;
             }
@@ -103,14 +85,6 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
     }
 
     void Onplay(){
-        
-        if(dice.activeSelf){
-        dice.GetComponent<slot_dice_script>().Start_bopping();
-        }
-        enable_play = false;
-        slot_dice_script.dice_moved = 1;
-        slot_dice_script.Yellow_selected = false;
-        slot_dice_script.White_selected = false;
         Invoke("delay",0.1f);
     }
 
@@ -119,14 +93,11 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
         {
         if(roll_dice.Ref_Array[slot_pos[0],slot_pos[1]] == dice.GetComponent<slot_dice_script>().dice_number)
             {
-            pixel.SetActive(true);
             dice.GetComponent<SpriteRenderer>().color = Color.green;
             //roll_dice.Ref_Array[slot_pos[0],slot_pos[1]] = 6;
             dice.GetComponent<slot_dice_script>().Correct_column = slot_pos[0];
             dice.GetComponent<slot_dice_script>().Correct_row = slot_pos[1];
             matched ++;
-            dice.GetComponent<slot_dice_script>().Stop_boping(); 
-            dice_script.dice_onboard --;  
             Invoke("pixel_delay", 1.0f);
             }
         else if(ColumnStateCheck() && (green_dice_count[slot_pos[0],dice.GetComponent<slot_dice_script>().dice_number] < Ref_board_dice_count[slot_pos[0],dice.GetComponent<slot_dice_script>().dice_number]))
@@ -153,24 +124,7 @@ public class slot_script : MonoBehaviour , IPointerDownHandler
     }
 
     void pixel_delay(){
-            
             pixel.GetComponent<SpriteRenderer>().color = pixel_array[slot_pos[0],slot_pos[1]];
             pixel.GetComponent<SpriteRenderer>().enabled = true;
-    }
-
-    void Update(){
-        if(slot_dice_script.Yellow_selected){
-            if(slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().parent_slot.GetComponent<slot_script>().slot_pos[0] == slot_pos[0] && slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().parent_slot.GetComponent<slot_script>().slot_pos[1] != slot_pos[1]){
-                greenborder.GetComponent<SpriteRenderer>().enabled = true;
-            }
-        }
-        else if(slot_dice_script.White_selected){
-            if(slot_dice_script.selected_slot_dice.GetComponent<slot_dice_script>().parent_slot.GetComponent<slot_script>().slot_pos[0] != slot_pos[0]){
-                greenborder.GetComponent<SpriteRenderer>().enabled = true;
-            }
-        }
-        else{
-            greenborder.GetComponent<SpriteRenderer>().enabled = false;
-            }
     }
 }
